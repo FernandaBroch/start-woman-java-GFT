3906702a18b8231d275b2a485ac2349a954406d4
FernandaBroch <fernanda.broch@gmail.com>
2022-05-27 21:48:03 -0300

    Add DataTypes

---
+++ src/main/java/JavaFundamental/variables/DataType.java
@@ -0,0 +1,35 @@
+package JavaFundamental.variables;
+
+public class DataType {
+
+    //Number
+    byte byteExample = 10;
+    byte byteExample2 = 20;
+
+    short shortExample = 20;
+    //short shortExample2 = 200000;
+
+    int intExample = 30;
+    //int intExample2 = -3000000000000;
+
+    long longExample = 3008909999887766009L;
+    long longExample2 = 300890987787868699L;
+
+    float floatExample = 3.456335674f;
+    //float floatExample2 = 3.45f;
+
+    double doubleExample = 3.14567;
+    double doubleExample2 = 30.14567;
+
+    //Text
+    char charExample = 'A';
+    char charExample2 = '\u0057';
+
+    String stringExample = "String Example";
+
+    //Logic
+    boolean booleanExample = true;
+    boolean booleanExample2 = false;
+
+
+}
